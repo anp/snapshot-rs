18c1306369a3f1fa3dc7252c412a4ee811b1e575
Adam Perry <adam.n.perry@gmail.com>
2017-07-23 14:40:44 -0700

    Add basic clap cli to cargo-snapshot.

--- cargo-snapshot/src/main.rs
+++ cargo-snapshot/src/main.rs
@@ -1,3 +1,17 @@
+#[macro_use]
+extern crate clap;
+
+use clap::{Arg, SubCommand};
+
 fn main() {
+    let input = app_from_crate!()
+        .subcommand(
+            SubCommand::with_name("update")
+                .about("Update snapshots.")
+                .arg(Arg::with_name("all").short("a").help(
+                    "Unconditionally update all snapshots, including unrecorded ones.",
+                )),
+        )
+        .get_matches();
     println!("Hello, world!");
 }
