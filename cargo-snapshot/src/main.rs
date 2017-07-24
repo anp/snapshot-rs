0a5cb4aed8665a2ee4de125e1193b553368adf1d
Adam Perry <adam.n.perry@gmail.com>
2017-07-23 23:06:43 -0700

    Handle unconditional updates and use error-chain.

--- cargo-snapshot/src/main.rs
+++ cargo-snapshot/src/main.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate clap;
 extern crate dialoguer;
+#[macro_use]
+extern crate error_chain;
 extern crate serde;
 extern crate serde_json;
 extern crate snapshot;
@@ -16,7 +18,19 @@ use dialoguer::{Checkboxes, Select};
 use snapshot::SnapFileContents;
 use walkdir::WalkDir;
 
-fn main() {
+error_chain!{
+    types {
+        Error, ErrorKind, ResultExt, SnapResult;
+    }
+
+    foreign_links {
+        Io(::std::io::Error);
+    }
+}
+
+quick_main!(run);
+
+fn run() -> SnapResult<()> {
     let input = app_from_crate!()
         .subcommand(
             SubCommand::with_name("update")
@@ -27,9 +41,37 @@ fn main() {
         )
         .get_matches();
 
-    let cwd = ::std::env::current_dir().expect("can't access working directory!");
+    let update_matches = input.subcommand_matches("update");
 
-    // throw away errors
+    if let Some(update_matches) = update_matches {
+        if update_matches.is_present("all") {
+            let status = Command::new("cargo")
+                .arg("test")
+                .env("UPDATE_SNAPSHOTS", "1")
+                .stdout(Stdio::null()) // FIXME(dikaiosune) capture w JSON output
+                .stderr(Stdio::null()) // FIXME(dikaiosune) and print nice message
+                .status()
+                .chain_err(|| "unable to execute cargo")?;
+
+            if !status.success() {
+                bail!("unable to update all snapshots!");
+            }
+        } else {
+            interactive_process()?;
+        }
+        println!("\nAll updates processed!");
+    } else {
+        panic!("unsupported command");
+    }
+
+    Ok(())
+}
+
+fn interactive_process() -> SnapResult<()> {
+    let cwd = ::std::env::current_dir()
+        .chain_err(|| "unable to read cwd")?;
+
+    // FIXME(adam) don't just throw away errors;
     let existing: Vec<SnapFileContents> = WalkDir::new(cwd)
         .into_iter()
         .map(|r| r.expect("unable to traverse project directory"))
@@ -37,12 +79,6 @@ fn main() {
         .map(|p| BufReader::new(File::open(p.path()).expect("unable to open snapshot file")))
         .map(|r| serde_json::from_reader(r).expect("unable to parse snapshot file"))
         .collect::<Vec<_>>();
-
-    let update_matches = input.subcommand_matches("update");
-
-    if update_matches.is_none() {
-        panic!("unsupported command!");
-    }
 
     let mut test_function_names = Vec::new();
     for snap_file in existing {
@@ -69,11 +105,10 @@ fn main() {
         let first_run_status = Command::new("cargo")
             .arg("test")
             .arg(&real_test_fn)
-// uncomment these once we can get just the error message back from cargo/rust
-//            .stdout(Stdio::null())
-//            .stderr(Stdio::null())
+            .stdout(Stdio::null()) // FIXME(dikaiosune) these should be captured as machine readable
+            .stderr(Stdio::null()) // FIXME(dikaiosune) and just the tes
             .status()
-            .expect("unable to execute cargo");
+            .chain_err(|| "unable to execute cargo")?;
 
         if !first_run_status.success() {
             failed_tests.push(real_test_fn.to_owned());
@@ -103,8 +138,10 @@ fn main() {
                     .arg("test")
                     .arg(fn_to_update)
                     .env("UPDATE_SNAPSHOTS", "1")
+                    .stdout(Stdio::null()) // FIXME(dikaiosune) these should be captured as JSON
+                    .stderr(Stdio::null()) // FIXME(dikaiosune) and just the test output shown
                     .status()
-                    .expect("unable to execute cargo");
+                    .chain_err(|| "unable to execute cargo")?;
 
                 if run_status.success() {
                     run_test = false;
@@ -117,7 +154,7 @@ fn main() {
                     let selection = Select::new()
                         .items(&options)
                         .interact()
-                        .expect("unable to retrieve user input");
+                        .chain_err(|| "unable to retrieve user input")?;
 
                     if options[selection] == "Retry" {
                         continue;
@@ -126,14 +163,12 @@ fn main() {
                     } else if options[selection] == "Abort" {
                         ::std::process::exit(1);
                     } else {
-                        panic!("invalid menu selection")
+                        bail!("invalid menu selection")
                     }
                 }
             }
         }
 
-        println!("\nAll updates processed!");
+        Ok(())
     }
-
-    // TODO figure out how to handle un-recorded tests
 }
